bac44b26f4a056835a2c602728d3253e3787012f
Yannick W <dev@pizzaboi.de>
2022-09-07 14:28:45 +0200

    better examples

--- examples/simple_setup.rs
+++ examples/simple_setup.rs
@@ -1,3 +1,4 @@
+use std::time::{Duration, UNIX_EPOCH};
 use rocket::{Build, Rocket, launch};
 use rocket::fs::Options;
 use rocket_cc_file_server::{CCFileServer, CCOptionsBuilder};
@@ -17,10 +18,22 @@ fn rocket() -> Rocket<Build> {
         .is_public(Some(false))
         .build();
 
+    let expires_example = CCOptionsBuilder::builder()
+        .expires(Some(
+            |_file_name| {
+                //You can provide a simple closure or a normal function
+
+                //Calculate the expire date and return it as a valid string. This can be archived with the httpdate crate
+                httpdate::fmt_http_date(UNIX_EPOCH + Duration::from_millis(7_956_831_600_000))
+            }
+        ))
+        .build();
+
     rocket::build()
         //.attach(...)  //setup your rocket instance as usual
         //.mount(...)
         .mount("/assets", CCFileServer::new("assets/", options.clone(), Options::default())) //simply add this to create a FileServer with the options provided
         .mount("/img", CCFileServer::new("img/", options, Options::default())) //Options reusable for multiple FileServer
         .mount("/js", CCFileServer::new("js/", js_options, Options::default())) //You can also use different options for different filetypes/directories
+        .mount("/expire_example", CCFileServer::new("/files_that_expire", expires_example, Options::default()))
 }
